5f015f8649d4eb7448f08afe2f99c21e4a8024a8
Hardika Zalavadia <hardika.youcan@gmail.com>
2024-07-03 01:40:45 +0530

    done

--- src/main/java/dev/hardika/userAccessManagement/security/SecurityConfig.java
+++ src/main/java/dev/hardika/userAccessManagement/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package dev.hardika.userAccessManagement.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,16 +20,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .dispatcherTypeMatchers(HttpMethod.valueOf("/admin/**")).hasRole("ADMIN")
-                .dispatcherTypeMatchers(HttpMethod.valueOf("/users/**")).hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/users/**").hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(csrf -> csrf.disable()); // Disable CSRF for simplicity
+
         return http.build();
     }
 
